b31c6ff89d32e6caee890dc8594abd56ee2c6352
Nicolas Yazman <nicolasyazman@gmail.com>
2025-04-30 11:10:36 +0200

    Now uses Codecov to display the code coverage.

--- frontend/teamtaskmanager-gui/vite.config.js
+++ frontend/teamtaskmanager-gui/vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     coverage: {
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60
+      },
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
