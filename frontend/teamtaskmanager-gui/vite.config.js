b7f585fb09b7e580b58abac2cce4d489c5d87a29
Nicolas Yazman <nicolasyazman@gmail.com>
2025-04-30 10:35:30 +0200

    Added a github action to automatically generate report on push.

--- frontend/teamtaskmanager-gui/vite.config.js
+++ frontend/teamtaskmanager-gui/vite.config.js
@@ -13,7 +13,13 @@ export default defineConfig({
     vueDevTools(),
   ],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   },
   resolve: {
     alias: {
